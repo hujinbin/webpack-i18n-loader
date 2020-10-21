9a5216cae7fc7b3d494fb80a351912bf7e64afb7
hujinbin <437044048@qq.com>
2020-10-21 16:26:33 +0800

    feat:新增自动翻译功能【新增配置文件】

--- bin/i18n.js
+++ bin/i18n.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 require('colors');
 const packageInfo = require('../package.json');
-const program = require('commander');
+const commander = require('commander');
 const generate = require('../lib/generate');
+const translate = require('../lib/translate');
+const init = require('../lib/init');
 
-program.version(packageInfo.version, '-v, --version');
+commander.version(packageInfo.version, '-v, --version');
 
-program.command('generate [src]')
+commander.command('generate [src]')
   .description('对src目录下的vue/js文件进行中文提取并生成国际化资源文件，默认src为执行目录下的src目录')
   .option('-p, --filepath <filepath>', '设置国际化文件的路径，默认为执行目录下的src/locale目录，请务必设置一个单独的目录来放置国际化资源文件')
   .option('-f, --filename <filename>', '设置生成文件的文件名，默认为 zh_cn，会自动添加.js 后缀')
@@ -14,13 +16,24 @@ program.command('generate [src]')
     generate(src, {filepath, filename});
   });
 
-program.on('command:*', function () {
-  console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
+commander.on('command:*', function () {
+  console.error('Invalid command: %s\nSee --help for a list of available commands.', commander.args.join(' '));
   process.exit(1);
 });
 
+
+commander.command('init')
+.action(()=>{
+  init();
+})
+
+commander.command('translate')
+.action(()=>{
+
+})
+
 if (process.argv.length === 2) {
-  program.help();
+  commander.help();
 }
 
-program.parse(process.argv);
+commander.parse(process.argv);
