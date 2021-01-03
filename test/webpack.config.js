749b5ee202e288e5ce5531013f25c3e1e49bf36e
hujinbin <437044048@qq.com>
2021-01-03 14:50:39 +0800

    test: 增加测试用户框架

---
+++ test/webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+let webpackMainVersion = 0;
+if(webpack.version){
+	webpackMainVersion = parseInt(webpack.version);
+}
+
+const config = {
+	entry:{
+		'index':'./index',
+	},
+	module:{
+		
+	},
+	output:{
+		library:'bundle',
+		libraryTarget:'commonjs2',
+		path: path.join(__dirname, '.'),
+		filename:'[name].js'
+	}
+};
+
+if(webpackMainVersion < 4){
+	config.module.loaders = [{
+        test: /\.vue$/,
+		loader: 'vue-loader',
+		options: {
+			exposeRoot: true
+		}
+       }];
+}else{
+	config.mode = 'development';
+	config.resolveLoader = {
+		alias: {
+			'webpack-in-loader': path.join(__dirname, '../index.js')
+		}
+	},
+	config.module.rules = [
+		 {
+        test: /\.vue$/,
+		loader: 'vue-loader',
+		options: {
+			exposeRoot: true
+		}
+       },];
+}
+
+
+module.exports = config;
