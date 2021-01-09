737aad60aafa8fe64265ad39af98cd44b759a61e
hujinbin <437044048@qq.com>
2021-01-09 16:07:53 +0800

    test: .vue文件的测试用例

---
+++ test/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import test from './test.vue'
+
+
+new Vue({
+    el: '#app',
+    components: { test },
+    template: '<test/>'
+});
---
+++ test/test.vue
@@ -0,0 +1,13 @@
+<template>
+   <div id="app">你好，世界</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
