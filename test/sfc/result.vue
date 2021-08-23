914192c2288c41da605f310139f76c372a64ab92
hujinbin <437044048@qq.com>
2021-08-23 18:01:27 +0800

    test: 补充测试用例

---
+++ test/sfc/result.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ $t('0') }}<div :value="$t('1')" :val="abc + $t('2') + ' afb'">{{ $t('3') }}</div>{{ $t('4') }}
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        const test = this.$t('5')
+    }
+}
+</script>
+
+<style>
+
+</style>
