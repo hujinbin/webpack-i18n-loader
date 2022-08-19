bee5926f0579ec25b8ad43c41bd5ff13c1fbc12c
hujinbin <437044048@qq.com>
2022-08-19 18:05:36 +0800

    feat: 增加vue2的使用案例

---
+++ examples/vue2/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import i18n from './i18n';
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  i18n,
+  router,
+  components: { App },
+  template: '<App/>'
+})
