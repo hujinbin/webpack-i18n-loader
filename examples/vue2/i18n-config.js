2ddc81a040899d232de50a6b1778dba40cb3a18d
hujinbin <437044048@qq.com>
2023-04-21 16:37:50 +0800

    feat: 新增 chatgpt方式方式,初始化文件增加注释信息

--- examples/vue2/i18n-config.js
+++ examples/vue2/i18n-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-	"dir": "./src/locale/",
-	"file": "zh.js",
-	"distLangs": [
-		"en",'jp','th','pt'
-	],
-	"open": true,
-	"appId": "", // 百度翻译appid
-	"secret": "" // 百度翻译密钥
+    dir: "./src/locale/", // 目标目录
+    file: 'zh.js', // 翻译的文件
+    distLangs: ['en'], // 要翻译的语言
+    open: true, // leader是否启用 默认true
+    mode:'chatGPT', //翻译方式: 1.Baidu:百度翻译 2:chatGPT:openai翻译
+    appId:'', // 百度翻译appid
+    secret:'', // 百度翻译密钥
+    openAiKey: '', // chatGPT key
+    singleNum: 3000, //单次请求最长次数
 };
