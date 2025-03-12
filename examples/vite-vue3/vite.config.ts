36201a41b5bca74e9e3c14ae248e2dab1f53bd96
hujinbin <437044048@qq.com>
2025-03-12 16:29:53 +0800

    feat: vite插件

--- examples/vite-vue3/vite.config.ts
+++ examples/vite-vue3/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import commonjs from 'vite-plugin-commonjs';
-import { i18nPlugin } from 'webpack-in-loader';
+import { vitePluginI18n } from 'webpack-in-loader';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), i18nPlugin(),commonjs()],
+  plugins: [vue(), vitePluginI18n(), commonjs()],
 })
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const initMessages = ({
     messages = require(localeFile);
     //文件变化监听
     const isProduction = process.env.NODE_ENV === 'production'
-    if(!isProduction){
+    if (!isProduction) {
       fs.watchFile(localeFile, {
         interval: 1000
       }, _ => {
@@ -39,63 +39,63 @@ let state = false // 读取配置状态 确保项目启动只读取一次
 // 获取当前项目的vue版本
 const getVueVersion = () => {
   // 获取vue版本号
-   const packageFile = path.join(process.cwd(), 'package.json');
-   let package = {};
-    // 获取当前项目的package.json信息
-   if (fs.existsSync(packageFile)) {
-      package = require(packageFile);
+  const packageFile = path.join(process.cwd(), 'package.json');
+  let package = {};
+  // 获取当前项目的package.json信息
+  if (fs.existsSync(packageFile)) {
+    package = require(packageFile);
   }
   console.log("/n ===============")
   // 获取当前vue版本，默认 2
   const vueVersion = package.dependencies.vue || package.devDependencies.vue;
   console.log(vueVersion)
-  try{
+  try {
     const firstVersion = String(vueVersion).split('.')[0];
     const vueArr = String(firstVersion).match(/\d+/g);
     vue = vueArr.join('')
     console.log("vue================")
     console.log(vue)
-  }catch(e){
+  } catch (e) {
     vue = 2;
   }
 }
 
 async function webpackLoader(source) {
-  if(state === false){
+  if (state === false) {
     if (fs.existsSync(config_file)) {
       const processFile = path.join(process.cwd(), config_file);
       config = require(processFile);
-    } 
+    }
     // else {
     //   return source
     // }
   }
   state = true
   // 不在打包环境下生效
-  if(config.open === false && process.env.NODE_ENV !== 'production'){
+  if (config.open === false && process.env.NODE_ENV !== 'production') {
     return source
   }
   let options = loaderUtils.getOptions(this);
   options = Object.assign({
-    localeFile: path.join(process.cwd(), 'src/locale/zh_cn.js')
+    localeFile: path.join(process.cwd(), 'src/locale/zh.js')
   }, options);
   initMessages(options);
   if (!messages) return source;
 
-  if(vue === 0){ // 只执行一次
+  if (vue === 0) { // 只执行一次
     getVueVersion();
   }
- 
+
   let result = '';
-  if(this.resourcePath.indexOf('node_modules')>-1){
+  if (this.resourcePath.indexOf('node_modules') > -1) {
     return source
   }
   const fileSuffix = path.extname(this.resourcePath)
-  if (['.js','.jsx', '.ts','.tsx'].includes(fileSuffix)
-  && this.resourcePath.indexOf(path.parse(options.localeFile).dir) < 0) {
+  if (['.js', '.jsx', '.ts', '.tsx'].includes(fileSuffix)
+    && this.resourcePath.indexOf(path.parse(options.localeFile).dir) < 0) {
     //处理js文件
     result = replaceScriptContent(source);
-  }else if (fileSuffix === '.vue') {
+  } else if (fileSuffix === '.vue') {
     //处理vue文件
     result = source.replace(/(<template[^>]*>)((.|\n|\r)*)(<\/template>)/gim, (_, preTag, content, $3, afterTag) => {
       return `${preTag}${replaceTemplateContent(content)}${afterTag}`;
@@ -109,49 +109,96 @@ async function webpackLoader(source) {
   return result;
 };
 
-
-async function loadConfigFile(configFilePath) {
-  if (fs.existsSync(configFilePath)) {
-    const configModule = await import(configFilePath);
-    return configModule.default || configModule;
+const initViteMessages = (localeFile) => {
+  //如果资源数据不存在，但资源文件存在，则将资源文件载入到资源数据中
+  if (!messages && fs.existsSync(localeFile)) {
+    const messagesContent = fs.readFileSync(localeFile, 'utf8');
+    try {
+      const startIndex = messagesContent.indexOf('{');
+      const endIndex = messagesContent.lastIndexOf('}');
+      const configString = messagesContent.slice(startIndex, endIndex + 1);
+      // 解析为对象
+      messages = eval('(' + configString + ')');
+    } catch (e) {
+      messages = {}
+    }
   }
-  return {};
-}
-
+};
 
 // vite插件
-function i18nPlugin() {
+function vitePluginI18n() {
   return {
-    name: 'i18n-plugin',
-    async transform(code, id) {
-      if (state === false) {
-        const configFilePath = path.join(process.cwd(), 'i18n-config.js');
-        config = await loadConfigFile(configFilePath);
+    name: 'vite-plugin-i18n',
+    configureServer(server) {
+      const configFilePath = path.join(process.cwd(), 'i18n-config.js');
+      try {
+        const fileContent = fs.readFileSync(configFilePath, 'utf8');
+        // 提取配置对象部分
+        const startIndex = fileContent.indexOf('{');
+        const endIndex = fileContent.lastIndexOf('}');
+        const configString = fileContent.slice(startIndex, endIndex + 1);
+        // 解析为对象
+        config = eval('(' + configString + ')');
+      } catch (error) {
+        console.error('Error reading or parsing i18n-config.js:', error);
+        config = {}
       }
-      state = true;
+    },
+    async transform(code, id) {
       // 不在打包环境下生效
       if (config.open === false && process.env.NODE_ENV !== 'production') {
-        return code;
+        return {
+          code: code,
+          map: null,
+        }
       }
-      let options = loaderUtils.getOptions(this);
-      options = Object.assign({
-        localeFile: path.join(process.cwd(), 'src/locale/zh_cn.js')
-      }, options);
-      initMessages(options);
-      if (!messages) return code;
+      const dir = config.dir;
+      const file = config.file;
+      let localeFile = path.join(process.cwd(), 'src/locale/zh.js')
+      if (dir && file) {
+        localeFile = path.join(process.cwd(), dir + file);
+      }
 
+      initViteMessages(localeFile);
+      if (!messages) {
+        return {
+          code: code,
+          map: null,
+        }
+      }
       if (vue === 0) { // 只执行一次
         getVueVersion();
       }
 
       let result = '';
       if (id.indexOf('node_modules') > -1) {
-        return code;
+        return {
+          code: code,
+          map: null,
+        }
       }
-      console.log(resourcePath)
+      // console.log(id)
+      console.log("messages======")
+      console.log(messages)
       const fileSuffix = path.extname(id);
+      const relativePath = path.relative(path.parse(localeFile).dir, id);
+      if (!relativePath.startsWith('..')
+        && !path.isAbsolute(relativePath)) {
+        console.log("id=========================================")
+        console.log(id)
+        return {
+          code: code,
+          map: null,
+        }
+      }
+      console.log("relativePath======")
+      console.log(id)
+
       if (['.js', '.jsx', '.ts', '.tsx'].includes(fileSuffix)
-        && id.indexOf(path.parse(options.localeFile).dir) < 0) {
+
+      ) {
+        console.log(!relativePath.startsWith('..'))
+        console.log(!path.isAbsolute(relativePath))
         // 处理 js 文件
         result = replaceScriptContent(code);
       } else if (fileSuffix === '.vue') {
@@ -165,6 +212,7 @@ function i18nPlugin() {
       } else {
         result = code;
       }
+      // console.log(result)
       return {
         code: result,
         map: null,
@@ -176,4 +224,4 @@ function i18nPlugin() {
 
 // 条件导出
 module.exports = webpackLoader;
-module.exports.i18nPlugin = i18nPlugin;
+module.exports.vitePluginI18n = vitePluginI18n;
